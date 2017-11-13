ea944130760f566b6a7aff328b8b0a1ea86aa68c
Mike Fuchsman <mike.fuchsman@summitmediadesign.com>
2017-11-13 16:48:36 -0700

    handlebars

---
+++ data/taxcutsummary.js
@@ -0,0 +1,24 @@
+exports.tweetSummary = [{
+  "female_Tweeters": 32,
+  "male_Tweeters": 22,
+  "unidentified_Tweeters": 46,
+  "tweets_Processed": 46,
+  "tweet_Not_Processed": 54,
+  "total_Tweeters": 100,
+  "time_to_Read_Tweets": 52.481,
+  "total_Positive_Words": 11,
+  "total_Negative_Words": 42,
+  "total_Nuetral_Words": 1876
+},
+{
+  "female_Tweeters": 32,
+  "male_Tweeters": 22,
+  "unidentified_Tweeters": 46,
+  "tweets_Processed": 46,
+  "tweet_Not_Processed": 54,
+  "total_Tweeters": 100,
+  "time_to_Read_Tweets": 52.481,
+  "total_Positive_Words": 11,
+  "total_Negative_Words": 42,
+  "total_Nuetral_Words": 1876
+}];
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
+var data = require("./data/taxcut.js");
+var tabledata = require("./data/taxcutsummary.js");
 
 var port = process.env.PORT || 8080;
 var app = express();
@@ -11,10 +14,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.use(express.static("front-end"));
 
 app.get("/", function(req, res) {
-	res.sendFile("index.html");
+	res.sendFile("./front-end/index.html");
+});
+
+
+app.get('/reports', function (req, res, cb) {
+    var count = 0;
+   res.render("home", {
+    tweet: data.tweetArray[count],
+    table: tabledata.tweetSummary[count]
+  });
+
+ //   	if ($("#next").click()) {
+ //   		count = count +1;
+	// } else if ($("#previous").click()){
+	// 	count = count -1;
+	// }
+   
+
+});
+app.get("/api", function(req, res){
+	res.json(data.tweetArray[0]);	
+});
+
+app.get("/api/tables", function(req, res){
+	res.json(tabledata.tweetSummary);	
 });
 
 require("./routes/password-controller.js")(app);
